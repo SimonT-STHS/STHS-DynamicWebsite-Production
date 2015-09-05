b6c59d740e05c81481b87c09ea9721a40a35beb6
SimonT <sths@simont.info>
2015-09-05 12:23:58 -0400

    Create Footer.php

---
+++ Footer.php
@@ -0,0 +1,5 @@
+<br />
+<div class="footer">Website create by the <a href=""http://sths.simont.info"">SimonT Hockey Simulator (STHS)</a> for <?php echo $LeagueOwner; ?>.</a></div>
+</body>
+</html>
+
