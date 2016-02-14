485d39b530af6d214ba31f906f1c6f5df2801a86
SimonT <sths@simont.info>
2016-02-14 12:56:14 -0500

    Update Header.php

--- Header.php
+++ Header.php
@@ -1,9 +1,7 @@
-<?php $DatabaseFile = (string)"SIMON2-STHS.db"; ?>
+<?php $DatabaseFile = (string)"D:\WWW\V3Output\SIM3-SEA-STHS.db"; ?>
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en"><head>
-<title>PHP Title</title>
-<script type="text/javascript" src="SIMON2.js"></script>
+<script type="text/javascript" src="STHSMain.js"></script>
 <meta charset="utf-8" />
 <link href="STHSMain.css" rel="stylesheet" type="text/css" />
 <link href="SIMON2.css" rel="stylesheet" type="text/css" />
-</head><body>
