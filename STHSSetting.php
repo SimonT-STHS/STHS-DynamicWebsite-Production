37017ff2e3526dfabc2b06b22dd0128399a08cbf
SimonT <sths@simont.info>
2021-12-01 19:07:48 -0500

    Delete STHSSetting.php

--- STHSSetting.php
+++
@@ -1,9 +0,0 @@
-﻿<?php
-$DatabaseFile = (string)"D:\WWW\V4Output\STHSDemo-STHS.db";
-$CareerStatDatabaseFile = (string)"D:\WWW\V4Output\STHSDemo-STHSCareerStat.db";
-$NewsDatabaseFile = (string)"D:\WWW\V4Output\STHSDemo-STHSNews.db";
-$Cookie_Name = (string)"STHS-STHSDemo";
-$CookieTeamNumberKey = (string)"8Khs4ntr+1+9fcVym8zlyWzZR60W3FaNB28F1WIQtcsT1YuMGMgsxC5hqbtWzaht7hG3VLHPnDDYKvFNtcPedw==";
-$WebClientHeadCode = "<link href=\"STHSMain.css\" rel=\"stylesheet\" type=\"text/css\" />";
-If (file_exists("STHSMain-CSSOverwrite.css") == true){$WebClientHeadCode = $WebClientHeadCode . "<link href=\"STHSMain-CSSOverwrite.css\" rel=\"stylesheet\" type=\"text/css\" />";}
-?>
